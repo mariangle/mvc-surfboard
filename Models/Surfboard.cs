8cd93e9a56367701f9f52842c7ffdcbda0a5a89d
marianle <124585244+mariangle@users.noreply.github.com>
2023-09-08 15:16:09 +0200

    add: surfboard.cs and scaffolding

---
+++ Models/Surfboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlTypes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvc_surfboard.Models
+{
+    public class Surfboard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Thickness { get; set; }
+        public double Volume { get; set; }
+        public string Type { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public string? Equipment { get; set; }
+        [Display(Name = "Image")]
+        public string? ImgUrl { get; set; }
+    }
+}
