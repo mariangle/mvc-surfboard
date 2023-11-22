4ebe4e98f91ce5ba2fcd46c48408617da928cc19
Niclas Høgh <niclas.hoegh@gmail.com>
2023-11-22 11:49:44 +0100

    Image fix & Zoom func (js)

---
+++ Webshop/Web/MyBlazorShop.Web.BlazorWasm/wwwroot/js/ImageZoom.js
@@ -0,0 +1,22 @@
+﻿function zoomin() {
+    var myImg = document.getElementById("zoom_img");
+    //var currWidth = myImg.clientWidth;
+    //if(currWidth >= 1000){
+    //    alert("You’re fully zoomed in!");
+    //} else{
+    //    myImg.style.width = (currWidth + 100) + "px";
+    //} 
+    myImg.style.backgroundColor = "red";
+}
+
+function zoomout() {
+    var myImg = document.getElementById("zoom_img");
+    //var currWidth = myImg.clientWidth;
+    //if (currWidth >= 50) {
+    //    alert("That’s as small as it gets.");
+    //} else {
+    //    myImg.style.width = (currWidth - 100) + "px";
+    //}
+
+    myImg.style.backgroundColor = "blue";
+}
